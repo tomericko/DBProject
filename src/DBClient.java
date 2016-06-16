3f1bb827c38f38af51d741d0524c7585a8194e6d
roi <roi890@gmail.com>
2016-06-16 18:48:51 +0300

    GUI first version

--- src/DBClient.java
+++ src/DBClient.java
@@ -1,3 +1,6 @@
+
+
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
---
+++ src/ScreenController.java
@@ -0,0 +1,57 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by roi on 16/06/16.
+ */
+public class ScreenController {
+
+    @FXML
+    ImageView dbLogo;
+
+    @FXML
+    ImageView ddlLogo;
+
+    @FXML
+    RadioButton ddlFileRadioBtn;
+
+    @FXML
+    TextField ddlFileChooserTxt;
+
+    @FXML
+    Button ddlFileChooserBtn;
+
+    @FXML
+    RadioButton ddlQueryRadioBtn;
+
+    @FXML
+    TextField ddlQueryTxt;
+
+    @FXML
+    Button ddlSendBtn;
+
+    @FXML
+    ImageView dmlLogo;
+
+    @FXML
+    RadioButton dmlFileRadioBtn;
+
+    @FXML
+    TextField dmlFileChooserTxt;
+
+    @FXML
+    Button dmlFileChooserBtn;
+
+    @FXML
+    RadioButton dmlQueryRadioBtn;
+
+    @FXML
+    TextField dmlQueryTxt;
+
+    @FXML
+    Button dmlSendBtn;
+
+}
