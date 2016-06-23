95bbd550047c149c71552cd7f9b38ceed6bf9722
Tomericko <tomer2600@walla.com>
2016-06-23 15:02:06 +0300

    Docu 1

--- src/DBClient.java
+++ src/DBClient.java
@@ -1,4 +1,9 @@
-
+/***
+ *Adar Dorham 203537824 89-281-03
+ *Omer Forma 304823230 89-281-03
+ *Roi Peretz 203258041 89-281-04
+ *Tomer Rahamim 203717475 89-281-05
+ ***/
 
 
 import java.io.BufferedReader;
@@ -8,28 +13,36 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/* Present a DB client who connects to server (detailed by conf file),
- * sends queries to the server and replies the server result */
+/**
+ * DBClient class - responsible for creating and maintaining the connection to the
+ * database specify in the configuration file. Send the queries to the server
+ * and receive the data.
+ */
 public class DBClient {
 
     private Connection connect = null;
     private Statement statement = null;
 
+    //The driver
     public static final String dbClass = "com.mysql.jdbc.Driver";
 
-    /* constructor
-     * establish connection with server */
+    /**
+     * DBClient c'tor - create new DBClient , extract the database information from
+     * the conf.txt file and establish the connection with the server.
+     * @throws Exception
+     */
     public DBClient() throws Exception {
 
-		/* extract configuration from conf.txt file located locally in
-		 * current directory */
+		//Extract configuration from conf.txt file.
         File conf = new File("conf.txt");
         BufferedReader br = new BufferedReader(new FileReader(conf));
+        //Take the usrl for the server.
         String url = br.readLine();
+        //Take the user name and password for the database.
         String userName = br.readLine();
         String password = br.readLine();
 
-        // register JDBC driver
+        //Register JDBC driver
         Class.forName(dbClass);
 
         // Open connection
@@ -37,8 +50,12 @@ public class DBClient {
         statement = connect.createStatement();
     }
 
-    /* send DML query to server and return server's result, or
-     * in case of error - throw the server's exception */
+    /**
+     * Parse the dml query and send it to the server.
+     * @param query - the client query
+     * @return the result return from the database
+     * @throws Exception
+     */
     public String sendDMLQuery(String query) throws Exception {
         String result = "";
         ResultSet resultSet = statement.executeQuery(query);
@@ -46,6 +63,7 @@ public class DBClient {
 
         int columnsNumber = rsmd.getColumnCount();
 
+        //Parse the result
         for (int i = 1; i <= columnsNumber; i++) {
             if (i > 1)
                 result += ",  ";
@@ -65,14 +83,22 @@ public class DBClient {
         return result;
     }
 
-    /* send DDL query to update server's DB,
-     * in case of error - throw the server's exception */
+    /**
+     * Send the ddl query to the server.
+     * @param query - the client query
+     * @throws Exception
+     */
     public void sendDDLQuery(String query) throws Exception {
         statement.executeUpdate(query);
     }
 
 
-
+    /**
+     * Open a giving file with script and read his content. Separate the script text
+     * to commands and return the commands as a list.
+     * @param file - the script.
+     * @return the list of commands.
+     */
     public List<String> readScriptFromFile(File file){
         String content = new String();
         StringBuffer sb = new StringBuffer();
@@ -80,24 +106,19 @@ public class DBClient {
         try
         {
             FileReader fr = new FileReader(file);
-            // be sure to not have line starting with "--" or "/*" or any other non aplhabetical character
-
             BufferedReader br = new BufferedReader(fr);
-
+            //read all lines
             while((content = br.readLine()) != null)
             {
                 sb.append(content);
             }
             br.close();
-
-            // here is our splitter ! We use ";" as a delimiter for each request
-            // then we are sure to have well formed statements
+            //Separate each command with ';'
             String[] inst = sb.toString().split(";");
 
+            //Ignore spaces and new line
             for(int i = 0; i<inst.length; i++)
             {
-                // we ensure that there is no spaces before or after the request string
-                // in order to not execute empty statements
                 if(!inst[i].trim().equals(""))
                 {
                     commands.add(inst[i]);
